72782358b8dd2759635e3b1da7eeca526613d070
derrose5 <121789489+derrose5@users.noreply.github.com>
2023-09-24 19:08:54 +0800

    new

---
+++ 01/app/src/main/java/com/example/a01/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      //  TextView tv_one=findViewById(R.id.tv_one);
+      //  tv_one.setText("解压球");
+    }
+}
---
+++ 01/app/src/main/java/com/example/a01/MyTextview.java
@@ -0,0 +1,27 @@
+package com.example.a01;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class MyTextview extends TextView {
+    public MyTextview(Context context) {
+        super(context);
+    }
+
+    public MyTextview(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextview(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
